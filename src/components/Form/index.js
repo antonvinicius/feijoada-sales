263af18c9ad6bcf9f9b7944bbf8a54cf1bfb1e8b
Vinicius <antoniovrdacosta@gmail.com>
2020-10-17 12:17:11 -0400

    Add validation

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,9 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Title } from '../TitleElement'
 import { animateScroll } from 'react-scroll'
 import { Button } from '../ButtonElement'
+import MaskedInput from 'react-text-mask'
+
 import {
     FormContainer,
     FormWrap,
@@ -12,21 +14,50 @@ import {
 
 
 export default function Index() {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [phone, setPhone] = useState('')
+    const intialValues = { email: "", name: "", phone: "" }
+    const [formValues, setFormValues] = useState(intialValues)
+    const [formErrors, setFormErrors] = useState({})
+    const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const obj = {
-        name,
-        email,
-        phone
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({ ...formValues, [name]: value });
+    };
+
+    const validate = (values) => {
+        let errors = {}
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+        if (!values.email) {
+            errors.email = "Não pode estar em branco"
+        } else if (!regex.test(values.email)) {
+            errors.email = "Email inválido"
+        }
+        if (!values.name)
+            errors.name = "Não pode estar em branco"
+        if (!values.phone) {
+            errors.phone = 'Não pode estar em branco'
+        } else if (values.phone.length < 16) {
+            errors.phone = 'Celular não é válido'
+        }
+        return errors;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormErrors(validate(formValues));
+        setIsSubmitting(true);
     }
 
-    const form = useRef()
-    const handleSubmit = () => {
+    useEffect(() => {
+        if (Object.keys(formErrors).length === 0 && isSubmitting) {
+            submitForm();
+        }
+    }, [formErrors]);
+
+    const submitForm = () => {
         fetch(`https://hooks.zapier.com/hooks/catch/8742077/ogjuytj/`, {
             method: 'POST',
-            body: JSON.stringify(obj),
+            body: JSON.stringify(formValues),
         })
         window.open('https://pag.ae/7WuDVFQB2', '_blank');
         animateScroll.scrollToTop()
@@ -37,27 +68,47 @@ export default function Index() {
             <Title size={20}>Preencha Seus Dados</Title>
             <FormWrap name="formSection">
                 <Message>Para realizar a compra da feijoada, faça seu cadastro.</Message>
-                <form id="form" onSubmit={e => e.preventDefault() || handleSubmit()}>
+                <form id="form" onSubmit={handleSubmit}>
+                    {formErrors.name && (
+                        <span style={{color: 'red'}}>{formErrors.name}</span>
+                    )}
                     <FormLabel>Nome*</FormLabel>
                     <FormInput
                         id="name"
                         name="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
+                        value={formValues.name}
+                        onChange={handleChange}
                     />
+                    {formErrors.email && (
+                        <span style={{color: 'red'}}>{formErrors.email}</span>
+                    )}
                     <FormLabel>Email*</FormLabel>
                     <FormInput
                         id="email"
                         name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        value={formValues.email}
+                        onChange={handleChange}
                     />
+                    {formErrors.phone && (
+                        <span style={{color: 'red'}}>{formErrors.phone}</span>
+                    )}
                     <FormLabel>Celular*</FormLabel>
-                    <FormInput
+                    <MaskedInput
+                        mask={['(', /[1-9]/, /\d/, ')', ' ', /\d/, ' ', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+                        style={{
+                            background: "#FFFFFF",
+                            border: "1px solid #000000",
+                            borderRadius: "13px",
+                            width: "100%",
+                            padding: "10px 10px",
+                            marginBottom: "20px",
+                            marginLeft: "-10px"
+                        }}
+                        guide={false}
                         id="phone"
                         name="phone"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
+                        value={formValues.phone}
+                        onChange={handleChange}
                     />
                 </form>
             </FormWrap>
